dece81373e45c045a16a17035baecd2b46999140
Iamneo <techadmin@iamneo.ai>
2023-08-14 13:09:03 +0000

    Backup Commit

--- demo/src/test/java/glue/GilletteTests.java
+++ demo/src/test/java/glue/GilletteTests.java
@@ -17,9 +17,9 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+// import cucumber.api.java.en.Given;
+// import cucumber.api.java.en.Then;
+// import cucumber.api.java.en.When;
 // import io.cucumber.java.After;
 // import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
